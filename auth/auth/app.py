9575eb075072de6e30893fa23e99e8572001b350
mmskv <max.mishuk2014@gmail.com>
2023-02-27 21:42:12 +0300

    Rename main.py to app.py as it starts a Flask app
    Reorganize imports
    Fix error responses
    Set token lifespan to 10 minutes

--- auth/auth/app.py
+++ auth/auth/app.py
@@ -2,15 +2,16 @@ import datetime
 
 import jwt
 from authlib.integrations.flask_client import OAuth
-from flask import Flask, make_response, redirect, url_for, logging
+from flask import Flask, logging, make_response, redirect, url_for
 
-from auth.util import register_oauth_providers_from_config, is_user_authorized
 from auth.log import configure_default_logger
+from auth.util import is_user_authorized, register_oauth_providers_from_config
 
 configure_default_logger()
 
 app = Flask("auth")
 app.logger = logging.create_logger(app)
+app.config.from_prefixed_env()
 app.config.from_object("config")
 
 oauth = OAuth(app)
@@ -19,10 +20,10 @@ register_oauth_providers_from_config(oauth, app.config)
 
 @app.route("/login/<provider>")
 def login(provider):
-    if provider not in app.config["OAUTH_PROVIDERS"]:
-        return 501
+    if provider not in app.config["OAUTH_REGISTERED_PROVIDERS"]:
+        return "501 Not implemented", 501
 
-    redirect_uri = url_for(f"auth", provider=provider, _external=True)
+    redirect_uri = url_for("auth", provider=provider, _external=True)
     return oauth.create_client(provider).authorize_redirect(redirect_uri)
 
 
@@ -35,15 +36,16 @@ def auth(provider):
         app.logger.info("Login success for user %s", userinfo["email"])
     else:
         app.logger.info("Login failure for user %s", userinfo["email"])
-        return 403
+        return "403 Forbidden", 403
 
     jwt_token = jwt.encode(
         {
             "email": userinfo["email"],
-            "exp": datetime.datetime.utcnow() + datetime.timedelta(days=1),
+            "exp": datetime.datetime.utcnow() + datetime.timedelta(minutes=10),
         },
         app.config["JWT_SECRET_KEY"],
     )
+
     response = make_response(redirect("/"))
     response.set_cookie("accessToken", jwt_token)
     return response
