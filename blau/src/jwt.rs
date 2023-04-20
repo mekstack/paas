203c5d6afdc84750ca4bbe30af917232cc3f50e2
mmskv <max.mishuk2014@gmail.com>
2023-04-20 18:37:46 +0300

    feat: blau

---
+++ blau/src/jwt.rs
@@ -0,0 +1,29 @@
+const JWT_TTL_SECONDS: u64 = 600;
+
+#[derive(serde::Serialize)]
+struct Claims {
+    username: String,
+    exp: u64,
+}
+
+#[derive(Clone)]
+pub struct JwtEncoder {
+    secret_key: String,
+}
+
+impl JwtEncoder {
+    pub fn new(secret_key: String) -> Self {
+        JwtEncoder { secret_key }
+    }
+
+    pub fn encode(&self, username: String) -> Result<String, jsonwebtoken::errors::Error> {
+        let exp = jsonwebtoken::get_current_timestamp() + JWT_TTL_SECONDS;
+
+        let claims = Claims { username, exp };
+        jsonwebtoken::encode(
+            &jsonwebtoken::Header::default(),
+            &claims,
+            &jsonwebtoken::EncodingKey::from_secret(self.secret_key.as_ref()),
+        )
+    }
+}
