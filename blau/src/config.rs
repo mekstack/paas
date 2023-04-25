05711842eb69e9c6ac98eebf957bf19d41e13ad9
mmskv <max.mishuk2014@gmail.com>
2023-04-26 00:16:51 +0300

    feat(blau): k8s
    had to rename BLAU_PORT -> PORT because of kubernetes env collision

--- blau/src/config.rs
+++ blau/src/config.rs
@@ -14,7 +14,7 @@ pub struct Config {
 impl Config {
     pub fn from_env() -> Config {
         Config {
-            server_port: get_env_var("BLAU_PORT"),
+            server_port: get_env_var_or_default("PORT", "80"),
             client_id: ClientId::new(get_env_var("CLIENT_ID")),
             client_secret: Some(ClientSecret::new(get_env_var("CLIENT_SECRET"))),
             issuer_url: IssuerUrl::new(get_env_var("ISSUER_URL")).unwrap(),
@@ -26,4 +26,8 @@ impl Config {
 
 fn get_env_var(var_name: &str) -> String {
     env::var(var_name).unwrap_or_else(|_| panic!("Environment variable {} is unset", var_name))
+}
+
+fn get_env_var_or_default(var_name: &str, default_value: &str) -> String {
+    env::var(var_name).unwrap_or_else(|_| default_value.to_string())
 }
